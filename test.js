51bb25ccf19556b6d6839b79792cc241b2a0a732
bred91 <paneraf02@gmail.com>
2023-03-02 17:52:36 +0100

    test2

--- test.js
+++ test.js
@@ -5,3 +5,16 @@ function test() {
 }
 
 test();
+
+function test2(text) {
+    const testo = text;
+    const test3 = function(){
+        return testo;
+    }
+
+    return test3;
+}
+
+const test4 = test2("test");
+
+console.log(test4());
