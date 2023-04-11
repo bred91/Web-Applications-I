760a2e40b6b83d62388208eb4fab2c7da9765637
bred91 <paneraf02@gmail.com>
2023-04-11 17:52:51 +0200

    Lab 03 e 04

---
+++ Lab/04/films.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const FILMS = [
+    // Data Strucutre: id, title, favorite, watchDate, rating
+    [1, "Pulp Fiction", true, "2023-03-10", 5],
+    [2, "21 Grams", true, "2023-03-17", 4],
+    [3, "Star Wars", false],
+    [4, "Matrix", true],
+    [5, "Shrek", false, "2023-03-21", 3]
+  ];
